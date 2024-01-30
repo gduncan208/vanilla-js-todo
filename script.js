f32ad6ec5b3963a15e5f5e76d0f46ff0a61d9037
Gracie Duncan <gracielduncan@gmail.com>
2024-01-30 12:47:25 -0800

    Working On edit button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("Ready Captain");
 
-    document.getElementById('add-task').addEventListener('click', function(element, event) {
-    console.log(element);
+    document.getElementById('add-task').addEventListener('click', function(event) {
+    event.preventDefault();
     console.log(event);
     var taskValue = document.getElementById('new-task').value;
 
@@ -13,35 +13,58 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     });
+
+    var taskNumber = 0;
+
     // Add Task
     function addTask(taskValue) {
         var li = document.createElement('li');
+        li.setAttribute("id", `${taskNumber}`)
+
+        var taskDiv = document.createElement('div');
+        taskDiv.textContent = taskValue;
+        taskDiv.classList.add("task-label");
+        taskDiv.setAttribute("id", `task-label${taskNumber}`)
+        li.appendChild(taskDiv);
+       
         console.log(li);
-        li.textContent = taskValue;
+
 
      // Add Delete Button
+        var actions = document.createElement('div')
+        actions.classList.add("task-actions");
+
         var deleteButton = document.createElement('button')
         deleteButton.textContent = "ABORT"
-        deleteButton.onclick = function() {
+        deleteButton.type = "button";
+        deleteButton.onclick = function(event) {
             li.parentNode.removeChild(li);
-            console.log("Child Abortion")
+            actions.appendChild(deleteButton);
+
+            console.log("Abortion")
         }
-        li.appendChild(deleteButton);
+
+        actions.appendChild(deleteButton);
 
     // Add Edit Button  
         var editButton = document.createElement('button');
         editButton.textContent = "Edit"
-        editButton.onclick = function () {
+        editButton.type = "button"
+        editButton.onclick = function (event) {
             editTask(li, taskValue);
              console.log("Edit")
         }
-        li.appendChild(editButton);
+
+        actions.appendChild(editButton);
+
+        li.appendChild(actions);
         
         li.addEventListener('click', function () {
             completeTask(li);
         });
-        document.getElementById('task-list').appendChild(li);
 
+        document.getElementById('task-list').appendChild(li);
+        taskNumber++;
     }
 
     function completeTask(li) {
@@ -52,15 +75,31 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function editTask(li, taskValue) {
-
-        var editDiv = document.createElement("div");
-        editDiv.setAttribute("id", "edit-task");
-
+        var label = li.querySelector(".task-label");
+        var actions = li.querySelector(".task-actions");
         var editInput = document.createElement('input');
+        editInput.type = "text";
         editInput.value = taskValue;
-        editDiv.appendChild(editInput);
-        li.appendChild(editInput, li);
+        label.replaceChildren(editInput);
 
+        // Add Save Button  
+        var saveButton = document.createElement('button');
+        saveButton.textContent = "Save";
+        saveButton.type = "button";
+        saveButton.onclick = function(event){
+
+        };
+        console.log("Edit Saved")
+
+        // Add Cancel Button
+        var cancelButton = document.createElement('button');
+        cancelButton.textContent = "Cancel";
+        cancelButton.type = "button";
+        cancelButton.onclick = function(event) {
+
+        }
+        console.log("Edit Cancelled")
+        actions.replaceChildren(cancelButton);
+        actions.appendChild(saveButton);
     }
-
 });
